2ad38305cf3be0d8ed86697b6a08f8e50f134826
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-02-10 15:19:35 +0530

    added validations

--- src/firstPage/firstPage.tsx
+++ src/firstPage/firstPage.tsx
@@ -27,6 +27,9 @@ function FirstPage() {
             }
         }
     })
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState(0);
     const [inputTheme,setTheme] = useState(Theme1)
     function numCheck(e:any) {
         if(e.target.value<0){
@@ -34,30 +37,72 @@ function FirstPage() {
             toast.dismiss()
             toast("phone number can't be negative")
         }
-        else if (e.target.value>=1000000000)
+        else if (e.target.value>=1000000000&&e.target.value<=1000000000000)
+        {
+            setNumber(e.target.value);
+            document.getElementById("numError")!.style.visibility = "hidden";
             setTheme(Theme2)
+        }
         else
+        {
+            setNumber(0);
+            if(e.target.value==="")
+            document.getElementById("numError")!.style.visibility = "hidden";
+            else
+            document.getElementById("numError")!.style.visibility = "visible";
             setTheme(Theme1)
+        }
     }
     function nameCheck(e:any) {
-        if(e.target.value=="")
-            setTheme(Theme1)
+        if((/^[a-zA-Z]*$/).test(e.target.value)||e.target.value=="")
+        {
+            setName(e.target.value);
+            document.getElementById("nameError")!.style.visibility = "hidden";
+            if(e.target.value==="")
+                setTheme(Theme1)
+            else
+                setTheme(Theme2)
+        }
         else
-            setTheme(Theme2)
+        {
+            setName("")
+            document.getElementById("nameError")!.style.visibility = "visible";
+            setTheme(Theme1)
+        }
     }
     function emailCheck(e:any) {
-        if(e.target.value=="")
-            setTheme(Theme1)
+        if((/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/).test(e.target.value) || e.target.value==="")
+        {
+            setEmail(e.target.value);
+            document.getElementById("emailError")!.style.visibility = "hidden";
+            if(e.target.value==="")
+                setTheme(Theme1)
+            else
+                setTheme(Theme2)
+        }
         else
-            setTheme(Theme2)
+        {
+            setEmail("")
+            document.getElementById("emailError")!.style.visibility = "visible";
+            setTheme(Theme1)
+        }
     }
 
     return(<div id="infoBox">
         <h3>Enter Your Details</h3>
         <ThemeProvider theme={inputTheme}>
-        <TextField className="textField" onClick={nameCheck} onChange={nameCheck} color="success" id="outlined-basic" label="Name" variant="outlined" />
-        <TextField onClick={numCheck} onChange={numCheck} type="number" color="success" id="outlined-basic" label="Phone Number" variant="outlined" />
-        <TextField onClick={emailCheck} onChange={emailCheck} color="success" id="outlined-basic" label="Email" variant="outlined" />
+        <div>
+        <TextField fullWidth className="textField" onClick={nameCheck} onChange={nameCheck} color="success" id="outlined-basic" label="Name" variant="outlined" />
+        <p className="error" id="nameError">Name Can Only Have Alphabets</p>
+        </div>
+        <div>
+        <TextField fullWidth className="textField" onClick={numCheck} onChange={numCheck} type="number" color="success" id="outlined-basic" label="Phone Number" variant="outlined" />
+        <p className="error" id="numError">Enter a Valid 10-12 digit Number</p>
+        </div>
+        <div>
+        <TextField fullWidth className="textField" onClick={emailCheck} onChange={emailCheck} color="success" id="outlined-basic" label="Email" variant="outlined" />
+        <p className="error" id="emailError">Enter a Valid Email Address</p>
+        </div>
         <Button color="secondary" variant="contained">Save</Button>
         </ThemeProvider>
         <ToastContainer limit={2} theme="light" position="top-center"/>
