a11d4f872441e1582b3334b84cbc2de9ab9a9225
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-02-11 14:04:23 +0530

    added details box in second page

--- src/secondPage/secondPage.tsx
+++ src/secondPage/secondPage.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
+import "./secondPage.css"
 
-const name = localStorage.getItem("name") || ""
-const email = localStorage.getItem("email") || ""
-const number = localStorage.getItem("number") || ""
 function SecondPage () {
+    const name = localStorage.getItem("name") || ""
+    const email = localStorage.getItem("email") || ""
+    const number = localStorage.getItem("number") || ""
     const navHandler = useNavigate();
     useEffect(() =>{if(name&&email&&number)
     {}
@@ -13,6 +14,11 @@ function SecondPage () {
     navHandler('/',{state: {status:0}})
     })
     return<div>
+        <div id="detailsView">
+            <p><b>Name :</b> {name}</p>
+            <p><b>Number :</b> {number}</p>
+            <p><b>Email :</b> {email}</p>
+        </div>
         <ToastContainer limit={2} theme="light" position="top-center"/>
     </div>
 }
